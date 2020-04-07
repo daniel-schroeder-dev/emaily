7d0afce63ebd0014172b034ca01c61058379faa8
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-07 00:33:04 -0500

    Setup heroku config.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res, next) => {
   res.json({ hi: 'there' });
 });
 
-app.listen(5000);
+app.listen(PORT, () => {
+  console.log(`Express app up at port: ${PORT}`);
+});
