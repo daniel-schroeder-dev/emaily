4b38d9b509b3424431e48374d4b05633ccfdc2b4
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-07 22:49:45 -0500

    Get basic OAuth flow going.

--- index.js
+++ index.js
@@ -2,32 +2,31 @@ require('dotenv').config();
 
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oath20').Strategy;
+const morgan = require('morgan');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(morgan('dev'));
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http:/localhost:5000/auth/google/callback"
+    callbackURL: '/auth/google/callback',
   },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+  (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken, refreshToken, profile);
   }
 ));
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.get('/login', (req, res, next) => {
+  res.send('fail');
+});
 
 app.listen(PORT, () => {
   console.log(`Express app up at port: ${PORT}`);
