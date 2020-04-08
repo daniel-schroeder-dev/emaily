616d0d5906df56b01ad68f873835893d8b8579f8
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-08 12:37:50 -0500

    Move passport config to it's own module.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const morgan = require('morgan');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const authRouter = require('./routes/authRouter');
 
@@ -13,22 +12,13 @@ const PORT = process.env.PORT || 5000;
 app.use(morgan('dev'));
 app.use(passport.initialize());
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
-  },
-  (accessToken, refreshToken, profile, done) => {
-    done(null, { profile });
-  }
-));
+require('./services/passport');
 
 app.use('/auth/google', authRouter);
 
 app.get('/', (req, res, next) => {
   res.send('Homepage route');
 });
-
 
 app.listen(PORT, () => {
   console.log(`Express app up at port: ${PORT}`);
