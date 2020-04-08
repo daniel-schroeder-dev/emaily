91a2e6340bc2990146dfc2742ba42f509fd168db
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-08 18:52:07 -0500

    Add mongoose connection config.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const authRouter = require('./routes/authRouter');
 
@@ -13,6 +14,17 @@ app.use(morgan('dev'));
 app.use(passport.initialize());
 
 require('./services/passport');
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).catch(console.error);
+
+mongoose.connection.on('error', err => {
+  console.error(err);
+});
+
+mongoose.set('debug', true);
 
 app.use('/auth/google', authRouter);
 
