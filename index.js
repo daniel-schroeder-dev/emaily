14dec9a4aa4777e7fa6f8b28087769823f67e85d
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-07 21:04:08 -0500

    Add passport config for Google. Add dotenv and add Google secrets.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+require('dotenv').config();
+
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oath20').Strategy;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res, next) => {
-  res.json({ hi: 'there' });
-});
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http:/localhost:5000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 app.listen(PORT, () => {
   console.log(`Express app up at port: ${PORT}`);
