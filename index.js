e3dcc5f8d7d8ae21d46bd686c3708b6f0d4228ac
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-09 15:47:14 -0500

    Add db/connect module to handle MongoDB connection.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
-
 const authRouter = require('./routes/authRouter');
 
 const app = express();
@@ -14,17 +12,7 @@ app.use(morgan('dev'));
 app.use(passport.initialize());
 
 require('./services/passport');
-
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).catch(console.error);
-
-mongoose.connection.on('error', err => {
-  console.error(err);
-});
-
-mongoose.set('debug', true);
+require('./db/connect');
 
 app.use('/auth/google', authRouter);
 
